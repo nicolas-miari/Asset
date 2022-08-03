016b9d7346573cf4ca688610a159f136f7c7fc8a
Nicolás Miari <nicolasmiari@gmail.com>
2022-08-03 14:00:59 +0900

    Basic protocol definitions.

--- Sources/Asset/Asset.swift
+++ Sources/Asset/Asset.swift
@@ -1,6 +1,36 @@
-public struct Asset {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
-    }
+/**
+ Root protocol, defines the interface common to all asset types.
+ */
+public protocol Asset {
+  /// The human-readable name of the asset. Can be changed at will.
+  var name: String { get set }
+
+  /// Uniquely identifies the asset. Immutable, generated on creation.
+  var identifier: String { get }
 }
+
+/**
+ Defines the interface common to all metadata-only assets.
+ */
+public protocol PrimitiveAsset: Asset {
+
+}
+
+/**
+ Defines the interface common to all asset types that depend on a binary resource.
+ */
+public protocol BasicAsset: Asset {
+
+  /// Uniquely identifies the binary resource on which this asset depends.
+  var binaryResourceIdentifier: String { get }
+}
+
+/**
+ Defines the interface common to all asset types that depend on other assets.
+ */
+public protocol CompositeAsset: Asset {
+  var dependencies: [any Asset] { get set }
+}
+
